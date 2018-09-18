d43c0e5646f7b6a29c8dd6f85baa627002d1d1f0
vanessa9910 <42152026+vanessa9910@users.noreply.github.com>
2018-09-18 18:15:57 -0500

    Update Pointers.c

--- Pointers.c
+++ Pointers.c
@@ -5,18 +5,18 @@
 #define MAX 50
 int size;
  
-// Defining the stack structure
+// STACK
 struct stack {
    int arr[MAX];
    int top;
 };
  
-// Initializing the stack(i.e., top=-1)
+// INIT STACK
 void init_stk(struct stack *st) {
    st->top = -1;
 }
  
-// Entering the elements into stack
+// PUSH
 void push(struct stack *st, int num) {
    if (st->top == size - 1) {
       printf("\nStack overflow(i.e., stack full).");
@@ -26,7 +26,7 @@ void push(struct stack *st, int num) {
    st->arr[st->top] = num;
 }
  
-//Deleting an element from the stack.
+//POP
 int pop(struct stack *st) {
    int num;
    if (st->top == -1) {
