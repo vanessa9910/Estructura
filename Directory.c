49f12ab25eb62c522343b1fc55eefdaa3c686c81
vanessa9910 <42152026+vanessa9910@users.noreply.github.com>
2018-11-02 11:13:15 -0600

    Update Directory.c

--- Directory.c
+++ Directory.c
@@ -1,1 +1,42 @@
+//Perla Vanessa Jaime Gaytán             
+//A00344428
+#include<stdio.h>
+//Create a program that creates a directory structure in the computer. The program should accept the following commands:
+// mkdir - add node
 
+
+//ls - print
+
+//rm - delete node
+
+//find - find
+
+//cd - abrir carpeta
+
+
+//main
+int main(){
+  while (1){
+      char comand[6];
+      printf("Enter a comand: ");
+      scanf("%s",comand);
+      printf("\nComando: %s",comand);
+      if (comand == "mkdir"){
+      }
+      else if(comand == "ls"){
+      }
+      else if(comand == "rm"){
+      }
+      else if(comand == "find"){
+      }
+      else if(comand == "cd"){
+      }
+
+      else if(comand =="exit"){
+        return 0;
+      }
+      else{
+        printf("\nThe comand is not valid");
+      }
+  }
+}
